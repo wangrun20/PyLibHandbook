2940bde7354c42e7e514741a0fc7fcef083636a8
汪润 <wangrun20@mails.tsinghua.edu.cn>
2023-11-28 23:25:41 +0800

    [Add] t & F distribution

--- scientific_computing.py
+++ scientific_computing.py
@@ -26,6 +26,30 @@ def chis_ppf(p, n):
     return x
 
 
+def t_cdf(x, n):
+    """计算P(X<x), 其中X服从自由度为n的t分布"""
+    p = stats.t.cdf(x, df=n)
+    return p
+
+
+def t_ppf(p, n):
+    """计算x, 使得P(X<x)=p, 其中X服从自由度为n的t分布"""
+    x = stats.t.ppf(p, df=n)
+    return x
+
+
+def F_cdf(x, m, n):
+    """计算P(X<x), 其中X服从自由度为(m, n)的F分布"""
+    p = stats.f.cdf(x, m, n)
+    return p
+
+
+def F_ppf(p, m, n):
+    """计算x, 使得P(X<x)=p, 其中X服从自由度为(m, n)的F分布"""
+    x = stats.f.ppf(p, m, n)
+    return x
+
+
 def comb(n, m):
     """计算组合数C_n^m, 即从n个不同元素中取m个元素并成一组的取法总数"""
     c = math.comb(n, m)
